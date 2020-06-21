3f8614582e99ed754a26200e4f025cd0c805934c
Tamas Galffy <ezittgtx@gmail.com>
2020-06-21 22:26:46 +0200

    refactor: Make boastgl_window_t usable without struct keyword

--- include/boastgl/boastgl.h
+++ include/boastgl/boastgl.h
@@ -3,20 +3,20 @@
 
 #include "include/boaster/image.h"
 
-struct boastgl_window_t;
+typedef struct boastgl_window_t boastgl_window_t;
 
 int boastgl_init();
 void boastgl_terminate();
 
-struct boastgl_window_t *boastgl_window_create();
-void boastgl_window_destroy(struct boastgl_window_t *window);
+boastgl_window_t *boastgl_window_create();
+void boastgl_window_destroy(boastgl_window_t *window);
 
 void boastgl_window_push_image(
-    struct boastgl_window_t *window,
+    boastgl_window_t *window,
     boaster_image_t *image,
     float scale
 );
 
-int boastgl_window_is_open(struct boastgl_window_t *window);
+int boastgl_window_is_open(boastgl_window_t *window);
 
 #endif  // BOASTGL_BOASTGL_H_
